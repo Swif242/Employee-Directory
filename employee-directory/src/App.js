aa07fb292f79b7ee8b1e6035ae0dc70fb515f15e
Jeremy Nelson <swif242@hotmail.com>
2020-06-21 04:48:46 -0700

    ran create react app and removed unnessasary files

---
+++ employee-directory/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1> 
+        Hello Jeremy
+      </h1>
+    </div>
+  );
+}
+
+export default App;
